04aa27227134c3af5c728f67770d6b0828e2fb5c
EricRHolland <86615280+EricRHolland@users.noreply.github.com>
2021-11-09 16:45:32 -0600

    correcting labels

--- app.py
+++ app.py
@@ -24,9 +24,9 @@ st.markdown("Im able to create it in Atom and Spyder but spent a lot of time get
 st.markdown("Spyder, Atom, Git Desktop are working fine, I wasnt able to let the user choose their preferred model among SVR, RF, Linear")
 
 median_income_in1 = st.number_input("Enter your immediate area's median income in USD:")
-population_in1 = st.number_input("Enter your immediate area's population:")
-longitude_in1 = st.number_input("Enter your district longitude:")
-latitude_in1 = st.number_input("Enter your district latitude:")
+population_in1 = st.number_input("Enter your immediate area's population (max 6000):")
+longitude_in1 = st.number_input("Enter your district longitude betweeen -120 and -140:")
+latitude_in1 = st.number_input("Enter your district latitude between 25 and 45:")
 median_income_in = float(median_income_in1)/10000
 population_in = int(population_in1)
 longitude_in = int(longitude_in1)
