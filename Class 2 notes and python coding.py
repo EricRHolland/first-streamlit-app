c78aa1c02780b0ced818827f5677ee6675e83d56
EricRHolland <86615280+EricRHolland@users.noreply.github.com>
2021-11-10 20:25:57 -0600

    Going to demo

--- Class 2 notes and python coding.py
+++ Class 2 notes and python coding.py
@@ -1,5 +1,13 @@
 #Class 2
 #Feature engineering goes very far. Model will perform better with better data.
 # good data with good feature engineering will win you competitions. 
-#
+# xg boost and lightgbm are the best for winning competitions
+
+
+
+
+# Walmart 1.3 billion datapoints generated every day for Sales predictions. 
+#label encoding is one of the ways to join multiple items together into one model
+# BUT it calculates an output for each item separately. 
+#following along with teh code given by the professor here. 
 
