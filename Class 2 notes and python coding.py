097e4114e8ebcad906f891efd62febf1955ccec5
EricRHolland <86615280+EricRHolland@users.noreply.github.com>
2021-11-17 20:54:41 -0600

    C2 notes

--- Class 2 notes and python coding.py
+++ Class 2 notes and python coding.py
@@ -10,4 +10,9 @@
 #label encoding is one of the ways to join multiple items together into one model
 # BUT it calculates an output for each item separately. 
 #following along with teh code given by the professor here. 
-
+import mlflow
+from google.colab import drive
+drive.mount('/content/gdrive')
+# in the 5 separate models, group items together, each item run separately
+#slow moving items, fast moving items, how would you separate similar items ?
+# stores, category?, whats the reasoning?
